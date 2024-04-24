a9e6d2518e76f6a184c72d203bdd5de62ea225b0
unknown <amitbiswas9282@gmail.com>
2024-04-24 18:10:25 +0600

    first commit

---
+++ src/Components.jsx/AddCoffee.jsx
@@ -0,0 +1,9 @@
+const AddCoffee = () => {
+  return (
+    <div>
+      <h2 className="text-2xl">Add coffee</h2>
+    </div>
+  );
+};
+
+export default AddCoffee;
---
+++ src/Components.jsx/UpdateCoffee.jsx
@@ -0,0 +1,9 @@
+const UpdateCoffee = () => {
+  return (
+    <div>
+      <h2 className="text-2xl">Update coffee</h2>
+    </div>
+  );
+};
+
+export default UpdateCoffee;
